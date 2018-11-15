b8ab16d49a92045683c10b450548e7b2ae83cdf4
Unknown <sam.corbridge@gmail.com>
2018-11-15 11:01:45 -0700

    curve tool and Ahaatua progress

---
+++ Rigging and Scripting in Maya/Python Files/Create Curve based on Joints.py
@@ -0,0 +1,16 @@
+def bonesList():
+    sel = cmds.ls(sl = True)
+    relatives = cmds.listRelatives(sel[0], ad = True)
+    relatives.append(sel[0])
+    locs = []
+    for relative in relatives:
+        loc = cmds.spaceLocator()
+        cmds.matchTransform(loc, relative)
+        locs.append(loc)
+   
+    positionOrder = [cmds.pointPosition(loc) for loc in locs]
+    print positionOrder 
+    cmds.curve(p=positionOrder) 
+    
+bonesList()
+
